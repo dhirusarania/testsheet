1947c1acee2863dae7724e4f620ed1564ab5ddf1
dhirusarania <dhirusarania@gmail.com>
2020-01-27 18:52:21 +0530

    quill copy

--- plugins/quill-editor.js
+++ plugins/quill-editor.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import { VueEditor , Quill } from "vue2-editor";
+import { VueEditor, Quill } from "vue2-editor";
 
 Vue.component('vue-editor', VueEditor)
 
@@ -7,20 +7,20 @@ const Clipboard = Quill.import('modules/clipboard')
 const Delta = Quill.import('delta')
 
 class PlainClipboard extends Clipboard {
-    onPaste (e) {
-      e.preventDefault()
-      const range = this.quill.getSelection()
-      const text = e.clipboardData.getData('text/plain')
-      const delta = new Delta()
-      .retain(range.index)
-      .delete(range.length)
-      .insert(text)
-      const index = text.length + range.index
-      const length = 0
-      this.quill.updateContents(delta, 'silent')
-      this.quill.setSelection(index, length, 'silent')
-      this.quill.scrollIntoView()
+    onPaste(e) {
+        e.preventDefault()
+        const range = this.quill.getSelection()
+        const text = e.clipboardData.getData('text/plain')
+        const delta = new Delta()
+            .retain(range.index)
+            .delete(range.length)
+            .insert(text)
+        const index = text.length + range.index
+        const length = 0
+        this.quill.updateContents(delta, 'user')
+        this.quill.setSelection(index, length, 'user')
+        this.quill.scrollIntoView()
     }
-  }
-  
-  Quill.register('modules/clipboard', PlainClipboard, true)
+}
+
+Quill.register('modules/clipboard', PlainClipboard, true)
